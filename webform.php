160aa79fd43e885d8d3e2653a0729f282c4aae16
Roman Zimmermann <roman@more-onion.com>
2016-02-18 11:15:22 +0100

    Initial version of the component including the webform integration.

---
+++ webform.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * @file
+ * Webform module uk_postcode component.
+ */
+
+/**
+ * Implements _webform_defaults_[component]().
+ */
+function _webform_defaults_uk_postcode() {
+  return array(
+    'name' => 'Postcode',
+    'form_key' => 'postcode',
+    'pid' => 0,
+    'weight' => 0,
+    'value' => '',
+    'mandatory' => 0,
+    'extra' => array(
+      'width' => '',
+      'unique' => 0,
+      'disabled' => 0,
+      'title_display' => 0,
+      'description' => '',
+      'attributes' => array(),
+      'private' => FALSE,
+    ),
+  );
+}
+
+/**
+ * Implements _webform_edit_[component]().
+ */
+function _webform_edit_uk_postcode($component) {
+  $form['value'] = array(
+    '#type' => 'uk_postcode',
+    '#title' => t('Default value'),
+    '#default_value' => $component['value'],
+    '#description' => t('The default value of the field.') . theme('webform_token_help'),
+    '#size' => 60,
+    '#maxlength' => 127,
+    '#weight' => 0,
+  );
+  $form['display']['width'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Width'),
+    '#default_value' => $component['extra']['width'],
+    '#description' => t('Width of the textfield.') . ' ' . t('Leaving blank will use the default size.'),
+    '#size' => 5,
+    '#maxlength' => 10,
+    '#parents' => array('extra', 'width'),
+  );
+  $form['display']['disabled'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Disabled'),
+    '#return_value' => 1,
+    '#description' => t('Make this field non-editable. Useful for setting an unchangeable default value.'),
+    '#weight' => 11,
+    '#default_value' => $component['extra']['disabled'],
+    '#parents' => array('extra', 'disabled'),
+  );
+  $form['validation']['unique'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Unique'),
+    '#return_value' => 1,
+    '#description' => t('Check that all entered values for this field are unique. The same value is not allowed to be used twice.'),
+    '#weight' => 1,
+    '#default_value' => $component['extra']['unique'],
+    '#parents' => array('extra', 'unique'),
+  );
+  return $form;
+}
+
+/**
+ * Implements _webform_render_[component]().
+ */
+function _webform_render_uk_postcode($component, $value = NULL, $filter = TRUE) {
+  $node = isset($component['nid']) ? node_load($component['nid']) : NULL;
+
+  $element = array(
+    '#type' => 'uk_postcode',
+    '#title' => $filter ? _webform_filter_xss($component['name']) : $component['name'],
+    '#title_display' => $component['extra']['title_display'] ? $component['extra']['title_display'] : 'before',
+    '#default_value' => $filter ? _webform_filter_values($component['value'], $node) : $component['value'],
+    '#required' => $component['mandatory'],
+    '#weight' => $component['weight'],
+    '#description' => $filter ? _webform_filter_descriptions($component['extra']['description'], $node) : $component['extra']['description'],
+    '#attributes' => $component['extra']['attributes'],
+    '#theme_wrappers' => array('webform_element'),
+    '#translatable' => array('title', 'description'),
+  );
+
+  // Add an uk-postcode class for identifying the difference from normal textfields.
+  $element['#attributes']['class'][] = 'uk-postcode';
+
+  // Enforce uniqueness.
+  if ($component['extra']['unique']) {
+    $element['#element_validate'][] = 'webform_validate_unique';
+  }
+
+  if (isset($value)) {
+    $element['#default_value'] = $value[0];
+  }
+
+  if ($component['extra']['disabled']) {
+    if ($filter) {
+      $element['#attributes']['readonly'] = 'readonly';
+    }
+    else {
+      $element['#disabled'] = TRUE;
+    }
+  }
+
+  // Change the 'width' option to the correct 'size' option.
+  if ($component['extra']['width'] > 0) {
+    $element['#size'] = $component['extra']['width'];
+  }
+
+  return $element;
+}
+
+/**
+ * Implements _webform_display_[component]().
+ */
+function _webform_display_uk_postcode($component, $value, $format = 'html') {
+  return [
+    '#markup' => isset($value[0]) ? $value[0] : '',
+  ];
+}
+
+/**
+ * Implements _webform_table_component().
+ */
+function _webform_table_uk_postcode($component, $value) {
+  return check_plain(empty($value[0]) ? '' : $value[0]);
+}
+
+
+/**
+ * Implements _webform_csv_headers_component().
+ */
+function _webform_csv_headers_uk_postcode($component, $export_options) {
+  $header = array();
+  $header[0] = '';
+  $header[1] = '';
+  $header[2] = $component['name'];
+  return $header;
+}
+
+/**
+ * Implements _webform_csv_data_component().
+ */
+function _webform_csv_data_uk_postcode($component, $export_options, $value) {
+  return empty($value[0]) ? '' : $value[0];
+}
